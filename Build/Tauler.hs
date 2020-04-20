683acc719016788c6e2b28ac016c6946b4ea36f2
luke <llucpagesperez@gmail.com>
2020-04-20 18:41:05 +0200

    - Tauler working good

--- Build/Tauler.hs
+++ Build/Tauler.hs
@@ -7,15 +7,19 @@ module Tauler where
   data Casella = Casella Posicio Char -- Posicio, Tipus
                  deriving Show
 
+  listCaselles :: [Posicio] -> [Char] -> [Casella]
+  listCaselles pl cl = [Casella (pl !! i) (cl !! i) | i <- [0..(length pl -1)]]
+
+
   data Tauler = Tauler Int Int [Casella] -- nfiles, ncolumnes, caselles
                 deriving Show
 
 
   creaTauler :: Int -> Int -> [Char] -> Tauler -- nfiles, ncolumnes, llista de tipus
-  creaTauler x y tList = Tauler x y [Casella (pList !! i) (tList !! i) | i <- [0 .. (length pList-1)]]
-                  where pList = posList (Posicio 0 0) (Posicio (x-1) (y-1)) -- Llista de caselles de mida x y
+  creaTauler x y tList = Tauler x y (listCaselles pList tList)
+                       where pList = posList (Posicio 0 0) (Posicio (x-1) (y-1)) -- Llista de caselles de mida x y
 
 
-  mostra :: Tauler -> IO()
-  mostra (Tauler x y caselles) = do
+  mostraTauler :: Tauler -> IO()
+  mostraTauler (Tauler x y caselles) = do
     putStrLn (show caselles)
